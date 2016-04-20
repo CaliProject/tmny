44c175973083a955cb874301f5f3ab8f682203fd
= <cali@calicastle.com>
2016-04-20 10:07:17 +0800

    Quick tweak second time

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -2,9 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title') - 天美农业</title>
+
+    <link href="//cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//cdn.bootcss.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 
+    @include('layouts.nav')
+
+    @yield('content')
+
+    <script src="//cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
 </body>
 </html>
